0bd96c76187757efa9dd55272f53f5d34d105b06
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2023-11-14 22:49:50 -0300

    Select SQL, projection, search games by lista

---
+++ src/main/java/com/devsuperior/dslist/projections/GameMinProjection.java
@@ -0,0 +1,17 @@
+package com.devsuperior.dslist.projections;
+
+/* Consulta SQL Customizada no spring DATA JPA
+ * classe Projection de consultas gets para SQL da classe GameRepostory 
+ * Todos os metodos gets criados referem-se aos campos de projeção da consulta SQL da classe "GameRepostory"
+ * Atenção os nomes antes do prefixo get precisam ser exatamente iguais aos campos da projeção na consulta SQL
+ * */
+
+public interface GameMinProjection {
+	
+	Long getId();
+	String getTitle();
+	Integer getYear();
+	String getImgUrl();
+	String getShortDescription();
+	Integer getPosition();
+}
