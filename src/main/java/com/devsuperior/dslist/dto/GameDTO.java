9f9e2e5b03d34f970e6c81878dee44b24caf9eb5
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2023-11-14 21:49:54 -0300

    GET game by id

---
+++ src/main/java/com/devsuperior/dslist/dto/GameDTO.java
@@ -0,0 +1,99 @@
+package com.devsuperior.dslist.dto;
+
+import org.springframework.beans.BeanUtils;
+
+import com.devsuperior.dslist.entities.Game;
+
+public class GameDTO {
+	
+	private Long id;
+	private String title;
+	private Integer year;
+	private String genre;
+	private String platforms;
+	private Double score;
+	private String imgUrl;										// CONFIGURA CAMPO VARCHAR NO BANCO MAIOR QUE 250 CARACTERES, POR PADRÃO VEM 250
+	private String shortDescription;
+	private String longDescription;
+	
+	public GameDTO() {
+	}
+	
+	public GameDTO(Game entity) {
+		BeanUtils.copyProperties(entity, this);					// utilizando um classe especial do framework para facilitar a criação do objeto no contrutor, ela ja popula todos os atributos não sendo necessario chamar atributo por atributo para polupar o objeto
+	}
+	
+	//DTOs comumente não utilização os sets, mas como foi utilizado a classe "BeanUtils" no contrutor necessario ter os gets e sets
+		
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getPlatforms() {
+		return platforms;
+	}
+
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
+
+	public String getLongDescription() {
+		return longDescription;
+	}
+
+	public void setLongDescription(String longDescription) {
+		this.longDescription = longDescription;
+	}
+}
--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
@@ -15,8 +17,16 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)															//import de ve ser do spring não pode ser do jakarta, read only = true para não bloquear o campo para escrita uma vez que o DTO de retorno
+	public GameDTO findById(Long id) {
+		Game gameResult = gameRepository.findById(id).get();								// necessario incluir o ".get" no final por causa do tipo de retorno do metodo findById que é um optional
+		return new GameDTO(gameResult);		
+	}
+	
+	@Transactional(readOnly = true)															//import de ve ser do spring não pode ser do jakarta, read only = true para não bloquear o campo para escrita uma vez que o DTO de retorno
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();		
-	}
+	}	
+	
 }
