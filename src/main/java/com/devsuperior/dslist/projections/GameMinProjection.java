1cbceaae18095780be7540d49a800d625014886b
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2023-11-15 22:27:46 -0300

    Homolog OK

--- src/main/java/com/devsuperior/dslist/projections/GameMinProjection.java
+++ src/main/java/com/devsuperior/dslist/projections/GameMinProjection.java
@@ -10,7 +10,7 @@ public interface GameMinProjection {
 	
 	Long getId();
 	String getTitle();
-	Integer getYear();
+	Integer getGameYear();
 	String getImgUrl();
 	String getShortDescription();
 	Integer getPosition();
