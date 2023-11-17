7d3950ac45fb87e79040423bc35fc65d0354a4c6
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2023-11-16 22:11:20 -0300

    Move games on list

---
+++ src/main/java/com/devsuperior/dslist/dto/ReplacementDTO.java
@@ -0,0 +1,22 @@
+package com.devsuperior.dslist.dto;
+
+public class ReplacementDTO {
+	
+	private Integer sourceIdex;
+	private Integer destinationIdex;
+	
+	
+	public Integer getSourceIdex() {
+		return sourceIdex;
+	}
+	public void setSourceIdex(Integer sourceIdex) {
+		this.sourceIdex = sourceIdex;
+	}
+	public Integer getDestinationIdex() {
+		return destinationIdex;
+	}
+	public void setDestinationIdex(Integer destinationIdex) {
+		this.destinationIdex = destinationIdex;
+	}
+
+}
